650a5d17021662dc6314b08e26ebdc5043af3fff
Fahim Muneer <fahimmuneer313gmail.com>
2025-11-10 08:13:53 +0000

    Moved every database details from settings to .env file.

--- shop/settings.py
+++ shop/settings.py
@@ -113,8 +113,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
-        'USER': 'fahim',
-        'PASSWORD': '1234',
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
